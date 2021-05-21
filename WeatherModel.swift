dab31c3aed1f924a9a9dad76ae72ca7ce4935e42
Haley-1617 <alicelai2920@gmail.com>
2021-05-21 01:51:42 -0700

    Completed data fetching and delegate method

---
+++ WeatherModel.swift
@@ -0,0 +1,39 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by Haley Lai on 5/21/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherModel {
+    let conditionId: Int
+    let cityName: String
+    let temperature: Double
+    
+    var tempStr: String {
+        return String(format: "%.1f", temperature)
+    }
+    
+    var conditionName: String {
+        switch conditionId {
+        case 200...232:
+            return "cloud.bolt.rain"
+        case 300...321:
+            return "cloud.drizzle"
+        case 500...531:
+            return "cloud.rain"
+        case 600...622:
+            return "cloud.snow"
+        case 701...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        default:
+            return "cloud"
+        }
+    }
+    
+}
